6339782397715c72b19a2d44eed0ba52a8d9dd43
Ismail Mhamdi <mhamdiismail15@gmail.com>
2023-12-24 15:05:27 +0100

    dd

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDroplet,faWind,faSun } from '@fortawesome/free-solid-svg-icons'
+import { faDroplet,faWind,faTemperatureArrowUp,faTemperatureArrowDown} from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import search_icon from '../assets/search.png';
 import sun_icon from '../assets/sun.png';
@@ -28,10 +28,14 @@ export default function Weather() {
         const humidity =document.getElementsByClassName("humidity");
         const wind =document.getElementsByClassName("wind");
         const temp =document.getElementsByClassName("temp");
+        const tempmax =document.getElementsByClassName("tempmax");
+        const tempmin =document.getElementsByClassName("tempmin");
         const location =document.getElementsByClassName("location");
         humidity[0].innerHTML=data.main.humidity+"%";
         wind[0].innerHTML=Math.floor(data.wind.speed)+" km/h";
         temp[0].innerHTML=Math.floor(data.main.temp)+ "°C";
+        tempmax[0].innerHTML=`${Math.floor(data.main.temp_max)}°C`;
+        tempmin[0].innerHTML=Math.floor(data.main.temp_min)+ "°C";
         location[0].innerHTML=data.name;
         if(data.weather[0].icon==='01d' ){
             setWicon(sun_icon)
@@ -74,22 +78,32 @@ export default function Weather() {
     return(
         <>
        
-        <div className="col-xl-6 pb-4 pt-2 h-auto m-auto text-center container mt-4 rounded-3 bg-info ">
+        <div className="col-xl-6  pt-2 h-auto m-auto text-center container mt-4 rounded-3 bg-info ">
            <img src={logo_icon} alt="" width={'120px'} className="" />
             <div className="d-flex pb-5 justify-content-center gap-2 pt-4"> 
             
             <input type="text" placeholder="Search" className="d-flex focus-ring fs-5 bold cityInput  bg-light text-secondary py-2 ps-3 w-50 border-0 rounded-4" />
-                <div className="search-icon" style={{cursor:'pointer'}} onClick={ ()=>{search()}}>
+                <a className="search-icon d-block" href="https://chaiweather.vercel.app/" onClick={ ()=>{search()}}>
                     <img src={search_icon} className="d-flex justify-content-center align-items-center p-3 rounded-circle bg-light cursor-pointer" alt="" />
-                </div>
+                </a>
             </div>
             <div className=" d-flex justify-content-center mt-0 mb-2">
                 <img src={wicon} alt="" className="rounded-circle p-3 w-25 bg-dark"  />
             </div>
-            <div className="d-flex justify-content-center temp text-dark fs-1 fw-bold">
-                24°C
+            <div className="d-flex justify-content-center">
+                <div className="d-flex align-items-center fs-5">
+                    <p className="tempmax fw-bold">22°C</p>
+                    <FontAwesomeIcon icon={faTemperatureArrowUp} className="mb-3 ms-1" />
+                </div>
+                <div className="temp text-dark fs-1 fw-bold mx-5">20°C</div>
+                <div className="d-flex align-items-center fs-5">
+                    <p className="tempmin fw-bold">5°C</p>
+                    <FontAwesomeIcon icon={faTemperatureArrowDown} className="mb-3 ms-1 " />
+                </div>
+                
+                
             </div>
-            <div className="d-flex justify-content-center text-dark fs-3 fw-bold location">London</div>
+            <div className="d-flex justify-content-center text-dark fs-3 fw-bold location">Oujda</div>
             <div className="mt-4 text-dark d-flex justify-content-center">
                 <div className="m-auto d-flex align-items-start gap-2">
                 
@@ -97,7 +111,7 @@ export default function Weather() {
                     <div className="fs-4 fw-bolder">
                         <div className="humidity">64%</div>
                         <div className="fs-6 ">Humidity</div>
-                        
+        
 
                     </div>
                 </div>
@@ -111,7 +125,7 @@ export default function Weather() {
                 </div>
                 
             </div>
-
+            <div className="text-dark mt-3 fw-bold p-1"><p>© 2023 Ismail Mhamdi</p></div>
         </div>
         </>
     
